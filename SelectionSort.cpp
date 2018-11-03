c9b704372139794ea1b3b134c7e6ae99400dcf8d
Timothy Itodo <itodotimothy6@gmail.com>
2018-11-03 17:45:15 -0500

    Added algo for SelectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  SelectionSort
+//
+//  Created by Timothy Itodo on 10/18/18.
+//  Copyright © 2018 Timothy Itodo. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void selectionSort(int arr[], int n){
+    int smallest;
+    int indexMin; // index of min element
+    
+    for (int i = 0; i < n-1; i++){// n-1 because no need to do the last one
+        smallest = arr[i];
+        indexMin = i;
+        for (int j = i+1; j < n; j++){
+            if (smallest > arr[j]){
+                smallest = arr[j];
+                indexMin = j;
+            }
+        }
+        swap(arr[indexMin], arr[i]);
+    }
+}
+
+
+
+int main() {
+    
+    int arr[] = {4,5,2,0,100,23,50,-6};
+    selectionSort(arr, 8);
+    
+    for(int i = 0; i < 8; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
