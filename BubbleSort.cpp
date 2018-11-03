1619fb7f379729805b8acb64f80e86d3ea4cfc13
Timothy Itodo <itodotimothy6@gmail.com>
2018-11-03 17:47:40 -0500

    Added algo for Bubble Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  BubbleSort
+//
+//  Created by Timothy Itodo on 10/18/18.
+//  Copyright © 2018 Timothy Itodo. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n-1-i; j++){// n-1 because you cant swap the last element with anything and -1 becuase the last i are already sorted
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
+//you can make this more efficient by putting a flag to indicate the array is sorted to avoid redundant passes
+
+int main() {
+    int arr[] = {10,-3,0,23456,7,8,100};
+    bubbleSort(arr, 7);
+    
+    for(int i = 0; i<7; i++){
+        cout << arr[i] << " ";
+    }
+}
